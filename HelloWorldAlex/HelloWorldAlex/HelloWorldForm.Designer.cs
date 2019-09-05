2ebf5201c0c911dfd4bf86789f2e532c621f7daa
s465640 <54953677+s465640@users.noreply.github.com>
2019-09-05 13:08:22 -0400

    Added Hello World.

---
+++ HelloWorldAlex/HelloWorldAlex/HelloWorldForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace HelloWorldAlex
+{
+    partial class frmHelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGreeting = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picWonderWoman = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWonderWoman)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGreeting
+            // 
+            this.lblGreeting.AutoSize = true;
+            this.lblGreeting.BackColor = System.Drawing.Color.Black;
+            this.lblGreeting.Font = new System.Drawing.Font("Javanese Text", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGreeting.ForeColor = System.Drawing.Color.Yellow;
+            this.lblGreeting.Location = new System.Drawing.Point(118, -11);
+            this.lblGreeting.Name = "lblGreeting";
+            this.lblGreeting.Size = new System.Drawing.Size(656, 218);
+            this.lblGreeting.TabIndex = 0;
+            this.lblGreeting.Text = "Hello, World!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("BankGothic Md BT", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(281, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 68);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "by Alexandrea\r\n\r\n";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HelloWorldAlex.Properties.Resources.images;
+            this.pictureBox1.Location = new System.Drawing.Point(562, 176);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(237, 288);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // picWonderWoman
+            // 
+            this.picWonderWoman.Image = global::HelloWorldAlex.Properties.Resources.images;
+            this.picWonderWoman.Location = new System.Drawing.Point(-27, 159);
+            this.picWonderWoman.Name = "picWonderWoman";
+            this.picWonderWoman.Size = new System.Drawing.Size(302, 277);
+            this.picWonderWoman.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picWonderWoman.TabIndex = 2;
+            this.picWonderWoman.TabStop = false;
+            // 
+            // frmHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picWonderWoman);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblGreeting);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Name = "frmHelloWorld";
+            this.Text = "Hello World! by Aex";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWonderWoman)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGreeting;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picWonderWoman;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
