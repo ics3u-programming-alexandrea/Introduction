b6327f909b6c82efeab721598bce2a326e1b060d
s465640 <54953677+s465640@users.noreply.github.com>
2019-09-16 13:02:48 -0400

    MovingCat

---
+++ MovingCat/MovingCat/MovingGirlsForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MovingCat
+{
+    partial class frmMovingGirls
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuFile = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuGirls = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniGirls1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniGirls2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.picGirls = new System.Windows.Forms.PictureBox();
+            this.mnuFile.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGirls)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile,
+            this.mnuGirls});
+            this.mnuFile.Location = new System.Drawing.Point(0, 0);
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(800, 24);
+            this.mnuFile.TabIndex = 0;
+            this.mnuFile.Text = "File";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(180, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.MniExit_Click);
+            // 
+            // mnuGirls
+            // 
+            this.mnuGirls.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniGirls1,
+            this.mniGirls2});
+            this.mnuGirls.Name = "mnuGirls";
+            this.mnuGirls.Size = new System.Drawing.Size(42, 20);
+            this.mnuGirls.Text = "Girls";
+            // 
+            // mniGirls1
+            // 
+            this.mniGirls1.Name = "mniGirls1";
+            this.mniGirls1.Size = new System.Drawing.Size(180, 22);
+            this.mniGirls1.Text = "Girls1";
+            this.mniGirls1.Click += new System.EventHandler(this.MniGirls1_Click);
+            // 
+            // mniGirls2
+            // 
+            this.mniGirls2.Name = "mniGirls2";
+            this.mniGirls2.Size = new System.Drawing.Size(180, 22);
+            this.mniGirls2.Text = "Girls2";
+            this.mniGirls2.Click += new System.EventHandler(this.MniGirls2_Click);
+            // 
+            // picGirls
+            // 
+            this.picGirls.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.picGirls.Image = global::MovingCat.Properties.Resources.ppg;
+            this.picGirls.Location = new System.Drawing.Point(244, 143);
+            this.picGirls.Name = "picGirls";
+            this.picGirls.Size = new System.Drawing.Size(247, 185);
+            this.picGirls.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGirls.TabIndex = 1;
+            this.picGirls.TabStop = false;
+            // 
+            // frmMovingGirls
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picGirls);
+            this.Controls.Add(this.mnuFile);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MainMenuStrip = this.mnuFile;
+            this.Name = "frmMovingGirls";
+            this.Text = "MovingGirls";
+            this.Load += new System.EventHandler(this.FrmMovingGirls_Load);
+            this.mnuFile.ResumeLayout(false);
+            this.mnuFile.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGirls)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.ToolStripMenuItem mnuGirls;
+        private System.Windows.Forms.ToolStripMenuItem mniGirls1;
+        private System.Windows.Forms.ToolStripMenuItem mniGirls2;
+        private System.Windows.Forms.PictureBox picGirls;
+    }
+}
+
---
+++ MovingCat/MovingCat/MovingGirlsForm.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by: Alexandrea Udechukwu
+ * Created on: 16-September-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #8 - Name of Program
+ * This program exits the program when the exit menu item is clicked and displays cartoon characters depending on what menu item is clicked.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingCat
+{
+    public partial class frmMovingGirls : Form
+    {
+        public frmMovingGirls()
+        {
+            InitializeComponent();
+        }
+
+        private void MniExit_Click(object sender, EventArgs e)
+        {
+            //this.will close the program
+            this.Close();
+        }
+
+        private void FrmMovingGirls_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MniGirls1_Click(object sender, EventArgs e)
+        {
+            this.picGirls.Image = Properties.Resources.ppg;
+        }
+
+        private void MniGirls2_Click(object sender, EventArgs e)
+        {
+            this.picGirls.Image = Properties.Resources.ppg2;
+        }
+    }
+}
